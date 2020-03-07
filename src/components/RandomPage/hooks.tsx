696d38df863e34a3b0d40021ab7fcf9569fb06ff
MaxBrohman <maksimbrochman@gmail.com>
2020-03-07 23:27:08 +0300

    removed words id routing

--- src/components/RandomPage/hooks.tsx
+++ src/components/RandomPage/hooks.tsx
@@ -1,29 +1,16 @@
-import { useHistory, useLocation } from 'react-router-dom'
-import { useEffect } from 'react'
-
-import { getWordsWithRandom, getRoute } from '../../utils'
+import { getWordsWithRandom } from '../../utils'
 import { useAppContext } from '../../reducer'
 import { currentWordsUpdate } from '../../actions' 
 
 export const useWords = () => {
   const { 
     state: {
-      classWords,
       currentWord,
       currentWords,
       words,
     },
     dispatch,
   } = useAppContext()
-  const { id } = currentWord
-  const history = useHistory()
-  const location = useLocation()
-
-  useEffect(() => {
-    const currentWordPathname = getRoute(`${classWords}/${id}/`)
-    if (location.pathname !== currentWordPathname)
-    history.push(currentWordPathname)
-  }, [classWords])
 
   const nextBtnClickHandler = () => {
 
@@ -32,11 +19,10 @@ export const useWords = () => {
       : getWordsWithRandom(words)
       
     currentWordsUpdate(currentValues, dispatch)
-    history.push(getRoute(`${classWords}/${currentValues.currentWord.id}/`))
   }
 
   return {
     nextBtnClickHandler,
-    words,
+    currentWord,
   }
 }
--- src/components/RandomPage/index.tsx
+++ src/components/RandomPage/index.tsx
@@ -1,25 +1,21 @@
 import React, { Fragment } from 'react'
-import { Link, Route } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import { Arrow } from '../Arrow'
 import { WordCard } from '../WordCard'
-import { withWordCardUrl } from '../../hocs'
 import { Btn } from '../Btn'
 import { nextBtnCaption, nextBtnClassName } from '../../config'
 import { useWords } from './hooks'
 import { getRoute } from '../../utils'
 
 export const RandomPage = () => {
- const { nextBtnClickHandler } = useWords()
+ const { nextBtnClickHandler, currentWord } = useWords()
   return (
     <Fragment>
       <Link to={getRoute('search/')}>
         <Arrow className={'right'} />
       </Link>
-      <Route 
-        path={getRoute(`:classWords/:id/`)} 
-        component={withWordCardUrl(WordCard)} 
-      />
+      <WordCard currentWord={currentWord} />
       <div className='next-btn-wrapper d-flex justify-content-center'>
         <Btn
           onClick={nextBtnClickHandler} 
