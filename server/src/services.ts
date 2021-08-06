0e8f177361e91f1916e117ac9999b26f22ef7f3a
MaxBrohman <maksimbrochman@gmail.com>
2021-08-06 10:55:13 +0300

    models added

---
+++ server/src/services.ts
@@ -0,0 +1,14 @@
+import {WordModel} from './models';
+
+export const getWords = async ({
+  search,
+  skip,
+  limit,
+  userId,
+  searchPropertyName,
+}: { limit?: number; skip?: number; search?: string; userId: string; searchPropertyName?: string }) => {
+  const baseQuery = { user: userId  };
+  const query = search ? {...baseQuery, [searchPropertyName]: {$regex: search.trim().toLowerCase(), $options: 'i' } } : baseQuery;
+  const words = await WordModel.find(query, undefined, { skip, limit });
+  return words;
+};
