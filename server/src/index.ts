611474a7b95329b89ccba57d50eee34d36344c95
MaxBrohman <maksimbrochman@gmail.com>
2021-08-09 07:54:28 +0300

    eslint and prettier added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,13 @@
-import { ApolloServer} from 'apollo-server';
-import { resolvers } from './resolvers';
-import {readFileSync} from 'fs';
-import { resolve  } from 'path';
+import { ApolloServer } from 'apollo-server'
+import { readFileSync } from 'fs'
+import { resolve } from 'path'
+import { resolvers } from './resolvers'
 
-const typeDefs = readFileSync(resolve(__dirname, './schema.gql')).toString('utf-8');
+const typeDefs = readFileSync(resolve(__dirname, './schema.gql')).toString('utf-8')
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers })
 
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+  // eslint-disable-next-line no-console
+  console.log(`🚀  Server ready at ${url}`)
+})
--- server/src/models.ts
+++ server/src/models.ts
@@ -1,17 +1,19 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
-  email: {type: String, required: true},
-  password: {type: String, required: true},
-  words:[{type: mongoose.Schema.Types.ObjectId, ref: 'Word' }]
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    words: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Word' }],
 })
 
 const wordSchema = new mongoose.Schema({
-  word: {type: String, required: true},
-  pronouncing: String,
-  translation: String,
-  user: {type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  sex: String
-});
+    word: { type: String, required: true },
+    pronouncing: String,
+    translation: String,
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    sex: String,
+})
 
-export const WordModel = mongoose.model('Word', wordSchema);
+export const UserModel = mongoose.model('User', userSchema)
+
+export const WordModel = mongoose.model('Word', wordSchema)
