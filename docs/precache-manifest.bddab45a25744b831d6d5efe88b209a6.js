1afb47c9fbfd9f11926cf8ab2e5867ed0b22529f
MaxBrohman <maksimbrochman@gmail.com>
2022-02-10 15:53:27 +0200

    build updated

--- docs/precache-manifest.bddab45a25744b831d6d5efe88b209a6.js
+++ docs/precache-manifest.bddab45a25744b831d6d5efe88b209a6.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a87c5af6c35aead2b3cd2c1291308968",
+    "revision": "5bd75f43cb9ec5e7d71680ee623f6353",
     "url": "/learn-hebrew/index.html"
   },
   {
-    "revision": "169e746fc1a9877885c4",
+    "revision": "7260ed4a5ff56a3c100c",
     "url": "/learn-hebrew/static/css/main.3455a260.chunk.css"
   },
   {
-    "revision": "ff1b9b0d31e812fd3ce3",
-    "url": "/learn-hebrew/static/js/2.f2328476.chunk.js"
+    "revision": "78ed3f6b920dc7972ee2",
+    "url": "/learn-hebrew/static/js/2.2e2dc963.chunk.js"
   },
   {
-    "revision": "169e746fc1a9877885c4",
-    "url": "/learn-hebrew/static/js/main.a7a4ef95.chunk.js"
+    "revision": "7260ed4a5ff56a3c100c",
+    "url": "/learn-hebrew/static/js/main.fd3c9c57.chunk.js"
   },
   {
     "revision": "70ac039ed8670654bbea",
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/learn-hebrew/precache-manifest.d36df47cbacedc749d6f1acebf9f460d.js"
+  "/learn-hebrew/precache-manifest.bddab45a25744b831d6d5efe88b209a6.js"
 );
 
 self.addEventListener('message', (event) => {
